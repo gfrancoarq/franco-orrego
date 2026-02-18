a6aa5dba4f70f6c653455d3db3c87182b79fde63
Gustavo Franco <gfrancoarquitecto@gmail.com>
2026-02-18 01:29:53 -0300

    Configurando chat

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {}, // Aquí está el cambio clave para la versión 4
   },
 };
 
