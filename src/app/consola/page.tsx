22092e8ff9b4d621232ea344f891a6fc422cdc3d
Gustavo Franco <gfrancoarquitecto@gmail.com>
2026-02-18 00:48:05 -0300

    Configurando chat

--- src/app/consola/page.tsx
+++ src/app/consola/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect, useRef } from 'react';
 import { createClient } from '@supabase/supabase-js';
+import '../globals.css';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
